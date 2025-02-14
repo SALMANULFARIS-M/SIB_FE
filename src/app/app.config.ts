a8e1faddd727ab94d178d4ed5c64cb14ff8839de
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-02-14 15:41:59 +0530

    sib angular first setups

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './interceptors/auth.interceptor'
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [ {
+    provide: HTTP_INTERCEPTORS,
+    useValue: authInterceptor,
+    multi: true, // Allow multiple interceptors
+  },provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
+
