7a22871dbd5c57e327b02e81fef92e34dfc71006
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-04-17 22:03:30 +0530

    added preload pages for courses and colleges

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -27,6 +27,40 @@ export const serverRoutes: ServerRoute[] = [
     }
   },
   {
+    path: 'colleges/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      try {
+        const response = await fetch('https://sib-be.onrender.com/education/college-ids');
+        if (!response.ok) throw new Error('Failed to fetch college IDs');
+        const ids = await response.json();
+
+        if (!Array.isArray(ids)) throw new Error('Invalid response format');
+        return ids.map((id: string) => ({ id }));
+      } catch (error) {
+        console.error('Error fetching college IDs:', error);
+        return [];
+      }
+    }
+  },
+  {
+    path: 'courses/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      try {
+        const response = await fetch('https://sib-be.onrender.com/education/course-ids');
+        if (!response.ok) throw new Error('Failed to fetch course IDs');
+        const ids = await response.json();
+
+        if (!Array.isArray(ids)) throw new Error('Invalid response format');
+        return ids.map((id: string) => ({ id }));
+      } catch (error) {
+        console.error('Error fetching course IDs:', error);
+        return [];
+      }
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender, // Fallback for all other routes
   }
