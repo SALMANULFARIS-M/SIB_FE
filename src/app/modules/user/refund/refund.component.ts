86026bc3c9e786fa7d3dd1171db218531aadebca
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-05-15 12:37:26 +0530

    Added refund and cancellation page

--- src/app/modules/user/refund/refund.component.ts
+++ src/app/modules/user/refund/refund.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../../shared/user/nav/nav.component';
 import { FooterComponent } from '../../../shared/user/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-refund',
-  imports: [NavComponent,FooterComponent],
+  imports: [NavComponent,FooterComponent,RouterModule],
   templateUrl: './refund.component.html',
   styleUrl: './refund.component.css'
 })
